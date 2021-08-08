47be82e75bf1cd551dda7552f6514c9446da8b7d
vishwanath-singh2308 <vishwanathsingh.2308@gmail.com>
2021-08-08 18:26:31 +0530

    Accessing Http

--- app.js
+++ app.js
@@ -3,6 +3,5 @@ const request = require('request')
 const url = 'https://www.weatherapi.com/docs/weather_conditions.json'
 
 request({ url:url}, (error,response)=>{
-    // const data = JSON.parse(response.body)
     console.log(response)
 })
