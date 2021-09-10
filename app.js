9fde559d7228b3f79991c96d1243fbfe5c0a0987
vishwanath-singh2308 <vishwanathsingh.2308@gmail.com>
2021-09-10 23:07:01 +0530

    asynchronous basic

--- app.js
+++ app.js
@@ -1,7 +1,2 @@
-const request = require('request')
-
-const url = 'https://www.weatherapi.com/docs/weather_conditions.json'
-
-request({ url:url}, (error,response)=>{
-    console.log(response)
-})
+console.log('Starting');
+console.log('Stopping');
